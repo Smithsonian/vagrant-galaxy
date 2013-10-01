97972673a7bdac03ba358dc311103c98ffa8c5f4
Andrew Stewart <andrew.c.stewart@gmail.com>
2013-10-01 11:53:28 -0400

    moved galaxy-dist to /vagrant

--- provision_galaxy.sh
+++ provision_galaxy.sh
@@ -4,15 +4,16 @@ sudo apt-get -y install mercurial
 
 # install galaxy
 echo "installing galaxy..."
-hg clone https://bitbucket.org/galaxy/galaxy-dist/
-cd galaxy-dist
+export GALAXYPATH="/vagrant/galaxy-dist/"
+hg clone https://bitbucket.org/galaxy/galaxy-dist/ $GALAXYPATH
+cd $GALAXYPATH
 
 echo "updating to stable..."
 hg update stable
 
 # configure galaxy
 echo "configuring galaxy..."
-cp /vagrant/universe_wsgi.ini /home/vagrant/galaxy-dist/universe_wsgi.ini
+cp /vagrant/universe_wsgi.ini $GALAXYPATH/universe_wsgi.ini
 
 # start galaxy
 echo "running galaxy daemon..."
